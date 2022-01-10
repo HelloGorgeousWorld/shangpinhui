e28b44c210d2cb2b6a71a4b83ce4cb455cada9f1
liumengling <15171491513@163.com>
2022-01-10 16:22:25 +0800

    1.10

--- src/components/TypeNav/index.vue
+++ src/components/TypeNav/index.vue
@@ -3,21 +3,21 @@
     <div class="container">
       <div @mouseleave="leaveIndex">
         <h2 class="all">全部商品分类</h2> 
-        <div class="sort">
+        <div class="sort" @click="goSearch">
           <div class="all-sort-list2">
             <div class="item" v-for="(c1,index) in categoryList" :key="c1.categoryId" :class="{cur:index==curruntIndex}">
               <h3 @mouseenter="changeIndex(index)">
-                <a href="">{{c1.categoryName}}</a>
+                <a :data-categoryName="c1.categoryName" :data-category1Id="c1.categoryId">{{c1.categoryName}}</a>
               </h3>
               <div class="item-list clearfix" :style="{display:curruntIndex === index?'block':'none'}">
                 <div class="subitem" v-for="c2 in c1.categoryChild" :key="c2.categoryId">
                   <dl class="fore">
                     <dt>
-                      <a href="">{{c2.categoryName}}</a>
+                      <a :data-categoryName="c2.categoryName" :data-category2Id="c2.categoryId">{{c2.categoryName}}</a>
                     </dt>
                     <dd>
                       <em v-for="c3 in c2.categoryChild" :key="c3.categoryId">
-                        <a href="">{{c3.categoryName}}</a>
+                        <a :data-categoryName="c3.categoryName" :data-category3Id="c3.categoryId">{{c3.categoryName}}</a>
                       </em>
                     </dd>
                   </dl>
@@ -63,6 +63,26 @@ export default {
       }, 50),
       leaveIndex(){
         this.curruntIndex = -1
+      },
+      goSearch(event){
+        let element = event.target
+        let { categoryname, category1id, category2id, category3id} = element.dataset
+        console.log(element.dataset)
+        console.log(categoryname)
+        if(categoryname) {
+          let location = {name:'search'}
+          let query = {categoryName: categoryname}
+          if(category1id){
+          query.category1Id = category1id
+          }else if(category2id){
+            query.category2Id = category2id
+          }else{
+            query.category3Id = category3id
+          }
+          location.query = query
+          // 跳转路由
+          this.$router.push(location)
+        }
       }
     },
     mounted() {
